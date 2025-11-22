1b318ae2aadaec90cc1ef719ab71fd82504219e9
yuval0812 <yuval0812@gmail.com>
2025-11-22 11:29:49 +0200

    Upload full project with frontend

---
+++ src/main/java/com/myproject/trello_backend/TrelloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.trello_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/trello_backend/TrelloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.trello_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
